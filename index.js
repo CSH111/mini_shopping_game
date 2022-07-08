0f9360c867cefbb49b598af3e993e5309da29e5f
seongho <seongho6022@naver.com>
2022-07-08 23:43:57 +0900

    json 기반으로 기능 재구현

--- index.js
+++ index.js
@@ -1,78 +1,59 @@
 "use strict";
 function loadItems() {
-  return fetch("./data/data.json") //
+  return fetch("./data/data.json")
     .then((response) => response.json())
     .then((json) => json.items);
 }
 
 loadItems() //
   .then((items) => {
-    console.log(items);
-    // displayItems()
-    // setEventListener()
+    displayItems(items);
+    setEventListener();
   });
 
-const clothesBtns = document.querySelectorAll(".clothes");
-const items = document.querySelector(".items").children;
+function displayItems(items) {
+  const ul = document.querySelector(".items");
+  ul.innerHTML = createItemsHTML(items);
+}
 
-clothesBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    Array.from(items).forEach((item) => {
-      item.classList.add("hidden");
-    });
-    if (btn.classList.contains("t-shirtsBtn")) {
-      Array.from(items).forEach((item) => {
-        if (item.classList.contains("t-shirts")) {
-          item.classList.remove("hidden");
-        }
-      });
-    } else if (btn.classList.contains("pantsBtn")) {
-      Array.from(items).forEach((item) => {
-        if (item.classList.contains("pants")) {
-          item.classList.remove("hidden");
-        }
-      });
-    } else if (btn.classList.contains("skirtsBtn")) {
-      Array.from(items).forEach((item) => {
-        if (item.classList.contains("skirts")) {
-          item.classList.remove("hidden");
-        }
-      });
+function createItemsHTML(items) {
+  return items
+    .map(
+      (item) =>
+        `<li class="item ${item.type} ${item.color}">
+        <img src="${item.image}" alt="" />
+        <div>${item.gender}, ${item.size}</div>
+      </li>
+      `
+    )
+    .join("");
+}
+
+function setEventListener() {
+  const btns = document.querySelector(".indexBtn");
+  const logo = document.querySelector(".logo");
+  btns.addEventListener("click", (event) => onIndexBtnClick(event));
+  logo.addEventListener("click", showAll);
+}
+function showAll() {
+  const lis = document.querySelectorAll(".item");
+  lis.forEach((li) => li.classList.remove("hidden"));
+}
+
+function onIndexBtnClick(event) {
+  const value = event.target.dataset.value;
+  const lis = document.querySelectorAll(".item");
+  if (!value) {
+    return;
+  }
+  showSortedItems(lis, value);
+}
+function showSortedItems(lis, value) {
+  lis.forEach((li) => {
+    const classList = li.classList;
+    classList.add("hidden");
+    if (classList.contains(value)) {
+      classList.remove("hidden");
     }
   });
-});
-
-const colorBtns = document.querySelectorAll(".color");
-colorBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    Array.from(items).forEach((item) => {
-      item.classList.add("hidden");
-    });
-    if (btn.classList.contains("blueBtn")) {
-      Array.from(items).forEach((item) => {
-        if (item.classList.contains("blue")) {
-          item.classList.remove("hidden");
-        }
-      });
-    } else if (btn.classList.contains("yellowBtn")) {
-      Array.from(items).forEach((item) => {
-        if (item.classList.contains("yellow")) {
-          item.classList.remove("hidden");
-        }
-      });
-    } else if (btn.classList.contains("pinkBtn")) {
-      Array.from(items).forEach((item) => {
-        if (item.classList.contains("pink")) {
-          item.classList.remove("hidden");
-        }
-      });
-    }
-  });
-});
-
-const homeBtn = document.querySelector("header button");
-homeBtn.addEventListener("click", () => {
-  Array.from(items).forEach((item) => {
-    item.classList.remove("hidden");
-  });
-});
+}
