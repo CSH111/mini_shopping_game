b4b0e35b54ccc5a7d8fb18dcdcd545e102cb7840
seongho <seongho6022@naver.com>
2022-07-08 16:50:37 +0900

    json 데이터 생성

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 "use strict";
+function loadItems() {
+  return fetch("./data/data.json") //
+    .then((response) => response.json())
+    .then((json) => json.items);
+}
+
+loadItems() //
+  .then((items) => {
+    console.log(items);
+    // displayItems()
+    // setEventListener()
+  });
 
 const clothesBtns = document.querySelectorAll(".clothes");
 const items = document.querySelector(".items").children;
